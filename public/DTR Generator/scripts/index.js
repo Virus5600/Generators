e58ac454b6696da69f3fa7e133c27cffca8db8b6
Virus5600 <satchi5600@gmail.com>
2023-07-25 21:30:36 +0800

    Update 20230725 - Calendar Update
    Updated the calendar portion of the form and adjusted it a bit to make it functional. In addition to those, a refactorization of the JS codes were done. See changelog for details.
    
    [CHANGELOG]
    🟢 Added new JS codes to make the button functional
    🟡 Disabled the "Generate" button on load
    🟡 Refactored various functions and grouped them accordingly inside a JavaScript Object
    🟡 Updated JS files used from bootstrap to the bundled version
    🔴 Removed table's element in favor of dynamic insertion via JS

--- public/DTR Generator/scripts/index.js
+++ public/DTR Generator/scripts/index.js
@@ -10,54 +10,108 @@ $(document).ready(() => {
 		$(`#jobOrderList`).click();
 	});
 
-	$(`#jobOrderList`).on('change', displayContent);
+	$(`#jobOrderList`).on('change', DTR.displayContent);
+	$(`#reset`).on('click', PAGE.reset);
+
+	PAGE.initCalendarView();
 });
 
-const displayContent = (e) => {
-	let reader = new FileReader();
-	let content;
+const DTR = {
+	displayContent(e) {
+		let reader = new FileReader();
+		let content;
 
-	reader.onload = () => {
-		content = reader.result;
+		reader.onload = () => {
+			content = reader.result;
 
-		let list = [];
-		let toAppend = `<ul>`;
+			let list = [];
+			let toAppend = `<ul>`;
 
-		$.each(content.split("\n"), (k, v) => {
-			list.push(v);
-			toAppend += `<li>${v.replaceAll(`\t`, `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`)}</li>`;
+			$.each(content.split("\n"), (k, v) => {
+				list.push(v);
+				toAppend += `<li>${v.replaceAll(`\t`, `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`)}</li>`;
+			});
+
+			toAppend += `</ul>`;
+
+			$(`#listPreview`).html(toAppend);
+			DTR.previewDTR(list);
+		};
+
+		if (e.target.files.length > 0)
+			reader.readAsText(e.target.files[0]);
+	},
+	previewDTR(list) {
+		let dates = "";
+
+		for (let i = 1; i <= 31; i++) {
+			dates += `
+				<div class="dtr-row">
+					<div class="dtr-cell text-right">${i}</div>
+					<div class="dtr-cell border border-dark small">${i}</div>
+					<div class="dtr-cell border border-dark small">${i}</div>
+					<div class="dtr-cell border border-dark small font-italic font-weight-bold text-danger">BRK</div>
+					<div class="dtr-cell border border-dark small">${i}</div>
+					<div class="dtr-cell border border-dark small">${i}</div>
+					<div class="dtr-cell"></div>
+				</div>
+			`;
+		}
+
+		$(`#dtrSample`).append(dates);
+
+		list.forEach((v) => {
+
 		});
-
-		toAppend += `</ul>`;
-
-		$(`#listPreview`).html(toAppend);
-		previewDTR(list);
-	};
-
-	if (e.target.files.length > 0)
-		reader.readAsText(e.target.files[0]);
+	}
 };
 
-const previewDTR = (list) => {
-	let dates = "";
+const PAGE = {
+	reset(e) {
+		const reset = $(e.currentTarget);
+		const formInputs = $(`#informationForm textarea, input:not([name="days[]"])`);
+	},
+	initCalendarView() {
+		const target = $(`#dateRangeContainer`);
+		let toAppend = ``;
 
-	for (let i = 1; i <= 31; i++) {
-		dates += `
-			<div class="dtr-row">
-				<div class="dtr-cell text-right">${i}</div>
-				<div class="dtr-cell border border-dark small">${i}</div>
-				<div class="dtr-cell border border-dark small">${i}</div>
-				<div class="dtr-cell border border-dark small font-italic font-weight-bold text-danger">BRK</div>
-				<div class="dtr-cell border border-dark small">${i}</div>
-				<div class="dtr-cell border border-dark small">${i}</div>
-				<div class="dtr-cell"></div>
-			</div>
-		`;
+		for (i = 0; i < 31; i++) {
+			const isSeventh = i%7 == 0;
+
+			// Every seventh iteration, add a row
+			if (isSeventh) {
+				if (i == 1)
+					toAppend += `<tr>`;
+				else if (i == 24)
+					toAppend += `</tr>`;
+				else
+					toAppend += `
+						</tr>
+
+						<tr>
+					`;
+			}
+
+			toAppend += `
+				<td>
+					<div class="btn-group-toggle" data-toggle="buttons">
+						<label class="btn btn-outline-secondary w-100" id="day${i+1}">
+							<input type="checkbox" name="days[]">
+							${i+1}
+						</label>
+					</div>
+				</td>
+			`;
+		}
+
+		target.html(toAppend);
+
+		target.find(`label`).on(`click`, (e) => {
+			let obj = $(e.currentTarget);
+
+			obj.addClass(`active`)
+				.find(`input`)
+				.prop(`checked`);
+		});
 	}
-
-	$(`#dtrSample`).append(dates);
-
-	list.forEach((v) => {
-
-	});
 };
