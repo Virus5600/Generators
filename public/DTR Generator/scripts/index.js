fdc9b00224e6a3da18f126997eaa8b809abd7416
Virus5600 <satchi5600@gmail.com>
2023-08-10 14:48:09 +0800

    Update 20230810 - Commit e58ac45 Parity
    After the recent merge, some changes are overwritten. Those are addressed in this commit, along with some minor updates.
    
    [CHANGELOG]
    🟢 Added transition styling
    🟢 Included transition styling to general styles
    🟢 Added DTR.print() function (no codes yet) in preparation to its implementation
    🟡 Refactored DTR Gen's script, compiling all DTR related function under the DTR object
    🔴 Removed the "Animate CSS" comment from DTR Gen and String Gen

--- public/DTR Generator/scripts/index.js
+++ public/DTR Generator/scripts/index.js
@@ -13,12 +13,12 @@ $(document).ready(() => {
 		$(`#jobOrderList`).click();
 	});
 
-	$(`#jobOrderList`).on('change', displayContent);
+	$(`#jobOrderList`).on('change', DTR.displayContent);
 
 	$(document).on(`click`, `[data-dtr-toggle][data-dtr-displayed=true]`, (e) => {
 		let obj = $(e.currentTarget);
 
-		resetDTR();
+		DTR.reset();
 		$(`#jobOrderList`).change();
 	});
 
@@ -26,97 +26,161 @@ $(document).ready(() => {
 		window.location = window.location;
 	});
 
-	$(`#generate`).on(`click`, generateDTR)
+	// Adding a little animation on hover (for the footer buttons) //
+	// GENERATE BTN
+	$(`#generate:not([disabled])`).on(`mouseenter focus`, (e) => {
+		$($(e.currentTarget).find(`*:first`))
+			.addClass(`fa-spin`)
+			.css(`--fa-animation-duration`, `1.125s`)
+			.css(`--fa-animation-iteration-count`, 1)
+			.css(`--fa-animation-timing`, `ease-out`);
+	}).on(`mouseleave blur`, (e) => {
+		$($(e.currentTarget).find(`*:first`))
+			.removeClass(`fa-spin`)
+			.css(`--fa-animation-duration`, ``)
+			.css(`--fa-animation-iteration-count`, ``)
+			.css(`--fa-animation-timing`, ``);
+	});
+
+	// PRINT BTN
+	$(`#print:not([disabled])`).on(`mouseenter focus`, (e) => {
+		let obj = $($(e.currentTarget).find(`*:first`));
+		
+		obj.addClass(`fa-bounce`)
+			.css(`--fa-animation-duration`, `1.5s`)
+			.css(`--fa-animation-iteration-count`, 1)
+			.css(`--fa-bounce-jump-scale-x`, `1.5`)
+			.css(`--fa-bounce-jump-scale-y`, `1.5`)
+			.css(`--fa-bounce-land-scale-x`, `1`)
+			.css(`--fa-bounce-land-scale-y`, `1`);
+
+	}).on(`mouseleave blur`, (e) => {
+		$($(e.currentTarget).find(`*:first`))
+			.removeClass(`fa-bounce`)
+			.css(`--fa-animation-duration`, ``)
+			.css(`--fa-animation-iteration-count`, ``)
+			.css(`--fa-animation-iteration-count`, ``)
+			.css(`--fa-bounce-jump-scale-x`, ``)
+			.css(`--fa-bounce-jump-scale-y`, ``)
+			.css(`--fa-bounce-land-scale-x`, ``)
+			.css(`--fa-bounce-land-scale-y`, ``);
+	});
+	
+	// RESET BTN
+	$(`#reset:not([disabled])`).on(`mouseenter focus`, (e) => {
+		$($(e.currentTarget).find(`*:first`))
+			.addClass(`fa-spin fa-spin-reverse`)
+			.css(`--fa-animation-duration`, `.5s`)
+			.css(`--fa-animation-iteration-count`, 1)
+			.css(`--fa-animation-timing`, `ease-in-out`);
+	}).on(`mouseleave blur`, (e) => {
+		$($(e.currentTarget).find(`*:first`))
+			.removeClass(`fa-spin fa-spin-reverse`)
+			.css(`--fa-animation-duration`, ``)
+			.css(`--fa-animation-iteration-count`, ``)
+			.css(`--fa-animation-timing`, ``);
+	});
+
+	// BUTTON DISABLES
+	$(`#generate`).on(`click`, DTR.generate)
+		.prop(`disabled`, true);
+
+	$(`#print`).on(`click`, DTR.print)
 		.prop(`disabled`, true);
 });
 
-const displayContent = (e) => {
-	resetDTR();
+const DTR = {
+	displayContent(e) {
+		DTR.reset();
 
-	let reader = new FileReader();
-	let content;
+		let reader = new FileReader();
+		let content;
 
-	reader.onload = () => {
-		content = reader.result;
+		reader.onload = () => {
+			content = reader.result;
 
-		let list = [];
-		let toAppend = `<ul>`;
+			let list = [];
+			let toAppend = `<ul>`;
 
-		$.each(content.split("\n"), (k, v) => {
-			list.push(v);
-			toAppend += `<li>${v.replaceAll(`\t`, `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`)}</li>`;
-		});
+			$.each(content.split("\n"), (k, v) => {
+				list.push(v);
+				toAppend += `<li>${v.replaceAll(`\t`, `&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`)}</li>`;
+			});
 
-		toAppend += `</ul>`;
+			toAppend += `</ul>`;
 
-		$(`#listPreview`).html(toAppend);
-		previewDTR(list);
-	};
+			$(`#listPreview`).html(toAppend);
+			DTR.preview(list);
+		};
 
-	if (e.target.files.length > 0)
-		reader.readAsText(e.target.files[0]);
-};
+		if (e.target.files.length > 0)
+			reader.readAsText(e.target.files[0]);
+	},
+	preview(list) {
+		let dates = "";
 
-const previewDTR = (list) => {
-	let dates = "";
+		for (let i = 1; i <= 31; i++) {
+			let date = $(`#date-${i}`);
 
-	for (let i = 1; i <= 31; i++) {
-		let date = $(`#date-${i}`);
+			dates += `
+				<div class="dtr-row" data-date="${date.attr("id")}">
+					<div class="dtr-cell text-end px-1">${i}</div>
+					<div class="dtr-cell border border-secondary small" data-update>${date.prop(`checked`) ? "08:00" : ""}</div>
+					<div class="dtr-cell border border-secondary small" data-update>${date.prop(`checked`) ? "12:00" : ""}</div>
+					<div class="dtr-cell border border-secondary small fst-italic fw-bold text-danger" data-update>${date.prop(`checked`) ? "BRK" : ""}</div>
+					<div class="dtr-cell border border-secondary small" data-update>${date.prop(`checked`) ? "01:00" : ""}</div>
+					<div class="dtr-cell border border-secondary small" data-update>${date.prop(`checked`) ? "05:00" : ""}</div>
+					<div class="dtr-cell"></div>
+				</div>
+			`;
+		}
 
-		dates += `
-			<div class="dtr-row" data-date="${date.attr("id")}">
-				<div class="dtr-cell text-end px-1">${i}</div>
-				<div class="dtr-cell border border-secondary small" data-update>${date.prop(`checked`) ? "08:00" : ""}</div>
-				<div class="dtr-cell border border-secondary small" data-update>${date.prop(`checked`) ? "12:00" : ""}</div>
-				<div class="dtr-cell border border-secondary small fst-italic fw-bold text-danger" data-update>${date.prop(`checked`) ? "BRK" : ""}</div>
-				<div class="dtr-cell border border-secondary small" data-update>${date.prop(`checked`) ? "01:00" : ""}</div>
-				<div class="dtr-cell border border-secondary small" data-update>${date.prop(`checked`) ? "05:00" : ""}</div>
-				<div class="dtr-cell"></div>
-			</div>
-		`;
-	}
+		$(dates).insertAfter(`#dtrSample #dtrHeader`);
+		$(`[data-dtr-toggle]`).attr(`data-dtr-displayed`, `true`);
 
-	$(dates).insertAfter(`#dtrSample #dtrHeader`);
-	$(`[data-dtr-toggle]`).attr(`data-dtr-displayed`, `true`);
+		if ($(`#generate`).prop(`disabled`))
+			$(`#generate`).prop(`disabled`, false);
+	},
+	reset() {
+		for (let i = 1; i <= 31; i++) {
+			$(`div.dtr-row[data-date]`).remove();
+		}
+	},
+	generate(e) {
+		let target = $(`#generatedDTR`), printTarget = $(`#printContainer > .row`);
+		let toAppend = ``, toAppendPrint = ``;
 
-	if ($(`#generate`).prop(`disabled`))
-		$(`#generate`).prop(`disabled`, false);
-};
+		$(`#listPreview li`).each((k, v) => {
+			let name = $(v).text().trim();
 
-const resetDTR = () => {
-	for (let i = 1; i <= 31; i++) {
-		$(`div.dtr-row[data-date]`).remove();
-	}
-};
+			let DTR = $(`#dtrSample`).parent().clone()
+				.find(`#dtrSample`)
+				.removeAttr(`id`)
+				.removeProp(`id`)
+				.parent();
 
-const generateDTR = (e) => {
-	let target = $(`#generatedDTR`), printTarget = $(`#printContainer > .row`);
-	let toAppend = ``, toAppendPrint = ``;
+			dataAttr.forEach((attr) => {
+				let obj = DTR.find(`[${prefix}-${attr}]`);
 
-	$(`#listPreview li`).each((k, v) => {
-		let name = $(v).text().trim();
+				if (attr == 'name')
+					obj.text(name);
+				else
+					obj.text(`${attr}`);
 
-		let DTR = $(`#dtrSample`).parent().clone()
-			.find(`#dtrSample`)
-			.removeAttr(`id`)
-			.removeProp(`id`)
-			.parent();
-
-		dataAttr.forEach((attr) => {
-			let obj = DTR.find(`[${prefix}-${attr}]`);
-
-			if (attr == 'name')
-				obj.text(name);
-			else
-				obj.text(`${attr}`);
-
+			});
+			
+			toAppend += DTR.html();
+			toAppendPrint += `<div class="col-6">${DTR.html()}</div>`;
 		});
 		
-		toAppend += DTR.html();
-		toAppendPrint += `<div class="col-6">${DTR.html()}</div>`;
-	});
-	
-	target.html(toAppend)
-		.addClass(`py-3`);
-	printTarget.html(toAppendPrint);
+		target.html(toAppend)
+			.addClass(`py-3`);
+		printTarget.html(toAppendPrint);
+
+		if ($(`#print`).prop(`disabled`))
+			$(`#print`).prop(`disabled`, false);
+	},
+	print() {
+
+	}
 };
