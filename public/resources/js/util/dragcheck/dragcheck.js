d6db5dea4da2be4f73db7e06fcca106049476c4f
Karl Satchi Navida <satchi5600@gmail.com>
2023-11-03 17:04:58 +0800

    Update 20231103 - Dragcheck Update
    Continued working on the drag check script for the implementation of issues #7 and #8 .
    
    [CHANGELOG]
    🟢 Added more inline documentation for the Dragcheck class, variables, and methods
    🟢 Added hover callback to the Dragcheck's callback parameter
    🟢 Added missing parameter handler
    🟢 Added missing callback parameter handlers
    🟡 Updated DTR Gen's script to now use the under-development Dragcheck utility script
    🔴 Removed old Dragcheck script in DTR Gen's script

--- public/resources/js/util/dragcheck/dragcheck.js
+++ public/resources/js/util/dragcheck/dragcheck.js
@@ -1,3 +1,30 @@
+/**
+ * A class which allows the user to drag and check elements. This is useful for creating
+ * a group of elements which can be checked by dragging the mouse cursor over them. This,
+ * however, is only limited to the mouse buttons. This means that the user can only use
+ * the mouse buttons to check the elements.
+ * 
+ * Recommended keycodes are only limited to the following:
+ * - {@link Dragcheck.KEYCODES.LEFT}
+ * - {@link Dragcheck.KEYCODES.MIDDLE}
+ * - {@link Dragcheck.KEYCODES.RIGHT}
+ * - {@link Dragcheck.KEYCODES.BACK}
+ * - {@link Dragcheck.KEYCODES.FORWARD}
+ * 
+ * These buttons are listed in {@link Dragcheck.KEYCODES}.
+ * 
+ * @example
+ * // Creates a new Dragcheck instance
+ * let dragcheck = new Dragcheck({
+ * 	group: ".dragcheck",
+ * 	states: ["checked", "unchecked"],
+ * 	keyCode: Dragcheck.KEYCODES.RIGHT,
+ * 	callbacks: {
+ * 		enabled: () => { console.log("Dragcheck is on"); },
+ * 		disabled: () => { console.log("Dragcheck is off"); }
+ * 	}
+ * });
+ */
 export default class Dragcheck {
 	// PRIVATE VARIABLES
 	/**
@@ -9,6 +36,27 @@ export default class Dragcheck {
 	 * Defines what states are available. Defaults to the `boolean` values
 	 */
 	#states = [true, false];
+
+	/**
+	 * Defines the default state of the element group. The value of this variable is
+	 * one of the the index of the default state in the {@link Dragcheck#states} variable.
+	 * This defaults to `0` which is `false` in the default `states`.
+	 */
+	#defaultState = 0;
+
+	/**
+	 * Defines the current state of the element group. The value of this variable is
+	 * one of the values provided in the {@link Dragcheck#states} variable. If {@link Dragcheck#states}
+	 * is a callback function, then the value of this variable is removed, which is
+	 * `undefined`.
+	 */
+	#currentState;
+
+	/**
+	 * Defines the element which was clicked to trigger this `Dragcheck` instance.
+	 * This becomes `undefined` after the mouse button is released.
+	 */
+	#clickedElement;
 
 	/**
 	 * Defines what group of elements should this target. Allowed values are css selectors only.
@@ -34,23 +82,33 @@ export default class Dragcheck {
 	 */
 	static KEYCODES = {
 		/**
-		 * The left mouse button.
+		 * The left mouse button. This is the default key code.
+		 * ------------------------
+		 * **Value**: `1`
 		 */
 		LEFT: 1,
 		/**
 		 * The middle mouse button.
+		 * ------------------------
+		 * **Value**: `2`
 		 */
 		MIDDLE: 2,
 		/**
 		 * The right mouse button.
+		 * ------------------------
+		 * **Value**: `3`
 		 */
 		RIGHT: 3,
 		/**
 		 * An auxilary mouse button. This usually exists within gaming mouse devices.
+		 * ------------------------
+		 * **Value**: `4`
 		 */
 		BACK: 4,
 		/**
 		 * An auxilary mouse button. This usually exists within gaming mouse devices.
+		 * ------------------------
+		 * **Value**: `5`
 		 */
 		FORWARD: 5
 	};
@@ -59,34 +117,59 @@ export default class Dragcheck {
 	 * Creates an instance of `Dragcheck` using the options provided. Provided
 	 * parameter should be an object containing the following properties:
 	 * 
-	 * @param {String}	group		A string which will define the target elements. This should be a valid css selector.
-	 * @param {Array}	states		(optional) An array containing the new states available for the elements.
-	 * @param {Integer}	keyCode		(optional) An integer which will define which mouse button will trigger. See {@link Dragcheck#KEYCODES} for the available key codes.
-	 * @param {Object}	callbacks	(optional) An object containing the callback functions of `enabled` and `disabled`.
+	 * @param {String}			group		A string which will define the target elements. This should be a valid css selector.
+	 * @param {Array|Callback}	states		(optional) An array containing the new states available for the elements. A callback can also be used to return a state dynamically. If the callback is used, a parameter will be passed, which is the clicked element.
+	 * @param {Number}			defaultState (optional) An integer value which will define the current state of the element group. Defaults to `0` which is `false` in the default `states`.
+	 * @param {Number}			keyCode		(optional) An integer which will define which mouse button will trigger. See {@link Dragcheck#KEYCODES} for the available key codes.
+	 * @param {Object}			callbacks	(optional) An object containing the callback functions of `enabled`, `hover` and `disabled`. The `hover` callback function takes in a single parameter which is the element that is being hovered.
 	 * 
 	 * @see Dragcheck#KEYCODES
 	 */
 	constructor({
 		group,
 		states = [true, false],
+		defaultState = 0,
 		keyCode = 1,
 		callbacks = {
-			enabled: () => { console.log("Dragcheck is on"); },
-			disabled: () => { console.log("Dragcheck is off"); }
+			enabled: () => {},
+			hover: () => {},
+			disabled: () => {}
 		}
 	} = {}) {
+		if (!group) throw new Error("Dragcheck: No target group specified.");
+
+		if (!Object.keys(callbacks).includes("enabled")) callbacks.enabled = () => {};
+		if (!Object.keys(callbacks).includes("disabled")) callbacks.disabled = () => {};
+		
 		this.#group = group;
 		this.#states = states;
+		this.#defaultState = defaultState;
 		this.#keyCode = keyCode;
 		this.#callbacks = callbacks;
 
 		document.addEventListener("mousedown", (e) => {
-			let target = e.target;
-			let list = Array.from(document.querySelectorAll(this.#group));
+			let keyCode = e.which || e.keyCode;
+			if (keyCode !== this.#keyCode) return;
+			
+			this.#clickedElement = e.target;
+			this.enable();
+		});
 
-			if (list.find(el => el === target)) {
-				this.#enabled = true;
+		document.addEventListener("mousemove", (e) => {
+			let keyCode = e.which || e.keyCode;
+			if (this.#enabled && this.#keyCode === keyCode) {
+				let target = e.target;
+				let list = Array.from(document.querySelectorAll(this.#group));
+
+				if (list.find(el => el === target)) {
+					this.#callbacks.hover(target);
+				}
 			}
+		});
+
+		document.addEventListener("mouseup", (e) => {
+			this.#clickedElement = undefined;
+			this.disable();
 		});
 	}
 
@@ -124,6 +207,19 @@ export default class Dragcheck {
 
 	/**
 	 * Fetches and return the variable's value. The variable
+	 * defines the current state of the element group. The value
+	 * of this variable is one of the values provided in the {@link Dragcheck#states}
+	 * variable. If {@link Dragcheck#states} is a callback function, then the value
+	 * of this variable is removed, which is `undefined`.
+	 */
+	get currentState() {
+		if (this.#states instanceof Function)
+			return this.#states(this.#clickedElement);
+		return this.#currentState;
+	}
+
+	/**
+	 * Fetches and return the variable's value. The variable
 	 * defines the callbacks for both when the dragcheck is
 	 * enabled and disabled.
 	 * 
@@ -137,6 +233,37 @@ export default class Dragcheck {
 	/////////////
 	// SETTERS //
 	/////////////
+
+	/**
+	 * Sets this `Dragcheck` instance to the `enabled` state. This will also
+	 * call the `enabled` callback function.
+	 * 
+	 * @param {Number} state (optional) An integer value which will define the current state of the element group.
+	 */
+	enable(state) {
+		this.#enabled = true;
+
+		if (!isNaN(parseInt(state))) state = (parseFloat(state) % 1) >= .5 ? Math.ceil(state) : Math.floor(state);
+		else state = 0;
+
+		if (this.#states instanceof Function) this.#currentState = this.#states(this.#clickedElement);
+		else this.#currentState = this.#states[state];
+
+		this.#callbacks.enabled();
+	}
+
+	/**
+	 * Sets this `Dragcheck` instance to the `disabled` state. This will also
+	 * call the `disabled` callback function.
+	 */
+	disable() {
+		this.#enabled = false;
+
+		if (this.#states instanceof Function) this.#currentState = undefined;
+		else this.#currentState = this.#states[this.#defaultState];
+
+		this.#callbacks.disabled();
+	}
 
 	/**
 	 * Sets a new value for the variable.
