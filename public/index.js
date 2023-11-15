904345b1459b7c62f28d84a3d903034a0ed61e69
Virus5600 <satchi5600@gmail.com>
2023-11-16 01:20:57 +0800

    Update 20231115 - Some Technical Updates
    Various technical updates and changes such as more NPM scripts and directory path updates.
    
    [CHANGELOG]
    🟢 Added a portable application
    🟢 Added a dedicated icon file at the root of the project
    🟢 Added an artifact name to the build, allowing a portable rename upon build
    🟡 Refactored auto updater (W.I.P.)

--- public/index.js
+++ public/index.js
@@ -15,7 +15,7 @@ const createWindow = () => {
 		width: 650,
 		height: 1250,
 		frame: false,
-		icon: path.join(__dirname, '../resources/favicon.png'),
+		icon: 'icon.ico',
 		webPreferences: {
 			enableRemoteModule: true,
 			contextIsolation: false,
@@ -25,11 +25,6 @@ const createWindow = () => {
 
 	remote.enable(win.webContents);
 	win.loadFile('public/index.html');
-
-	// Auto Updater
-	win.once('ready-to-show', () => {
-		autoUpdater.checkForUpdatesAndNotify();
-	});
 };
 
 app.whenReady().then(() => {
@@ -38,6 +33,36 @@ app.whenReady().then(() => {
 	app.on('activate', () => {
 		if (BrowserWindow.getAllWindows().length === 0) {
 			createWindow();
+
+			// Auto Updater - Checks for update every time the app is opened.
+			// Currently at pre-release so this is always on
+			autoUpdater.allowPrerelease = true;
+
+			autoUpdater.on('update-available', (e) => {
+				console.log('update-available', e);
+				win.webContents.send('update-available', e);
+			});
+
+			autoUpdater.on('update-downloaded', () => {
+				console.log('update-downloaded');
+				win.webContents.send('update_downloaded');
+			});
+
+			autoUpdater.on('error', (e) => {
+				console.log('update-error', e);
+				win.webContents.send('update-error', e);
+			});
+
+			autoUpdater.on('download-progress', (e) => {
+				console.log('update-progress', e);
+				win.webContents.send('update-progress', e);
+			});
+
+			ipcMain.on('restart_app', () => {
+				autoUpdater.quitAndInstall();
+			});
+
+			autoUpdater.checkForUpdates();
 		}
 	});
 });
@@ -53,16 +78,4 @@ ipcMain.on('app_version', (e) => {
 	e.sender.send('app_version', {
 		version: app.getVersion()
 	});
-});
-
-ipcMain.on('restart_app', () => {
-	autoUpdater.quitAndInstall();
-});
-
-autoUpdater.on('update-available', () => {
-	win.webContents.send('update_available');
-});
-
-autoUpdater.on('update-downloaded', () => {
-	win.webContents.send('update_downloaded');
 });
