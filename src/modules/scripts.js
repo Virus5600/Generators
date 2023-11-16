e88b501e41ad7a466203c81697b2175ba401c0a5
Karl Satchi Navida <satchi5600@gmail.com>
2023-11-16 16:48:56 +0800

    Update 20231116 - Auto Updater Fix Attempt
    Fix attempt to the auto updater of Generators. Further test will be conducted down the line which will result to false releases for testing. Those release will be deleted after a test however.
    
    [CHANGELOG]
    🟢 Added Sweet Alert's dark theme
    🟢 Added a minimum size for the application
    🟢 Added the Configuration/Option at the frameless' header
    🟡 Updated editorconfig to now use CRLF instead of LF EOLs
    🟡 Updated several package.json values
    🟡 Refactored frameless in preparation to the implementation of the auto updater
    🟡 Updated Sweet Alert JS import
    🟡 Changed the @use directive to @import in the base style.scss
    🟡 Fixed an extension that was mistyped resulting to a JS file instead of CSS

--- src/modules/scripts.js
+++ src/modules/scripts.js
@@ -5,7 +5,7 @@ window.$ = window.jQuery = require("node_modules/jquery/dist/jquery.min.js");
 require("node_modules/bootstrap/dist/js/bootstrap.bundle.min.js");
 
 // Sweet Alert 2
-require("node_modules/sweetalert2/dist/sweetalert2.all.min.js");
+window.Swal = require("node_modules/sweetalert2/dist/sweetalert2.min.js");
 
 // Fontawesome
 require("./fontawesome.js");
--- webpack.mix.js
+++ webpack.mix.js
@@ -22,25 +22,25 @@ mix.webpackConfig({
 	.copy('src/js/util/', 'public/resources/js/util/', false)
 
 	.copy('src/css/', 'public/resources/css/', false)
-	
+
 	.copy('src/resources/', 'public/resources/', false)
-	
+
 	// Additional Styles
 	.sass('src/sass/general.scss', '/resources/css')
-	
+
 	// Landing Page Modules
 	.js('src/modules/scripts.js', '/resources/libs.js')
-	.sass('src/modules/styles.scss', '/resources/libs.js')
-	
+	.sass('src/modules/styles.scss', '/resources/libs.css')
+
 	// String Generator Modules
 	.js('src/modules/string-generator/scripts.js', 'String Generator/scripts/libs.js')
 	.sass('src/modules/string-generator/styles.scss', 'String Generator/styles/libs.css')
-	
+
 	// DTR Generator Modules
 	.js('src/modules/dtr-generator/scripts.js', 'DTR Generator/scripts/libs.js')
 	.sass('src/modules/dtr-generator/styles.scss', 'DTR Generator/styles/libs.css')
 	.sass('src/modules/dtr-generator/index.scss', 'DTR Generator/styles/index.css')
-	
+
 	// FINAL CONFIGURATIONS
 	.setPublicPath("public/")
 	.sourceMaps()
