aa1766ace8f3cc9bae445964c75b0fff53ad55f0
Karl Satchi Navida <satchi5600@gmail.com>
2023-11-26 05:48:22 +0800

    Issue #17 - String Generator Fix `undefined` Value (#19)
    * Update 20231123 - Compression Update
    
    Compressed and made the application smaller by moving all the NPM dependencies to the dev-dependencies. Furthermore, made the "About" page visible to The Hub.
    
    [CHANGELOG]
    🟢 Added a line which disables the menu setup of the application for optimization
    🟢 Added (little) proper content inside the About page
    🟢 Added a new submit handler for the form in String Generator
    🟡 Bumped version to 1.2.0-beta.1
    🟡 Moved all the unneeded code when compiled inside `devDependency` property
    🟡 Updated `files` property list to now exclude everything except for the main entry point and the public directory
    🟡 Updated extra resources directory to include all contents of src/resources
    🟡 Refactored the entire copy-pasted About page to use the updated library paths
    🔴 Removed PopperJS
    
    * Update 20231125 - Issue #17 Fix and More
    
    Fixed the `undefined` issue stated at #17. Some tweaking and further fixes are done to improve the overall dependency of String Generator.
    
    [CHANGELOG]
    🟢 Added some null/undefined handlers to some chained methods for STRG's scripts in accordance to Issue #17
    🟢 Added a text animation to the string being removed and generated. An option to disable this animation will be created on the next commit
    🟡 Bumped the minimum range that can be provided, from 0 to 1
    🟡 Updated some validation message to be more detailed on what the error might be
    🟡 Fixed the artificial window minimizer on form reset
    🟡 Fixed an issue where the validation rules are overwritten when being modified which results to some weird validation messages
    
    * Update 20231126 - Enhancement For #17
    
    As per one of the "Added" changelog from commit 496964d, the text animation now respects the "Reduced Motion" preference. This is rooted with issue #17.
    
    [CHANGELOG]
    🟢 Added documentation to the functions in the script
    🟢 Added `animateText()` function so that it can be called whenever and wherever it is needed instead of being a single time code
    🟡 Changed the `validate()` function to be hidden in the global scope by removing the `window.` prefix

--- src/modules/string-generator/index.js
+++ src/modules/string-generator/index.js
@@ -8,9 +8,9 @@ import Validator from "../../js/util/validator/Validator.js";
 var validation = {
 	rules: {
 		"use_range": ["sometimes", "boolean"],
-		"range_min": ["sometimes", "numeric", "min:0", "max:$range_max"],
+		"range_min": ["sometimes", "numeric", "min:1", "max:$range_max"],
 		"range_max": ["sometimes", "numeric", "min:$range_min", "max:255"],
-		"length": ["sometimes", "numeric", "between:0,255"],
+		"length": ["sometimes", "numeric", "between:1,255"],
 		"chars": ["required", "array", "min:1"],
 		"chars.*": ["sometimes", "in:alpha,numeric,spec"]
 	},
@@ -20,12 +20,13 @@ var validation = {
 		},
 		"range_min": {
 			"numeric": `Minimum range should be a number`,
-			"min": `Minimum value should be :min`,
-			"max": `Maximum value should be :max`,
+			"min": `Minimum value should be at least :min`,
+			"max": `Maximum value should be at max, :max`,
 		},
 		"range_max": {
-			"min": `Maximum value should be :min`,
-			"max": `Maximum value should be :max`,
+			"numeric": `Maximum range should be a number`,
+			"min": `Maximum value should be at least :min`,
+			"max": `Maximum value should be at max, :max`,
 		},
 		"length": {
 			"length.numeric": `Length should be a number`,
@@ -66,9 +67,9 @@ $(function() {
 	$(`#range_min`).on('change', (e) => {
 		let obj = $(e.currentTarget);
 		let target = $(`#range_max`);
-		let newMin = obj.val();
+		let newMin = obj.val() ?? 0;
 
-		newMin = newMin.match(/^-?\d+$/g).length > 0 ? parseInt(newMin) : parseInt(target.attr('min'));
+		newMin = (newMin.match(/^-?\d+$/g)?.length ?? 0) > 0 ? parseInt(newMin) : parseInt(target.attr('min'));
 
 		target.attr('min', newMin);
 	}).trigger('change');
@@ -77,9 +78,9 @@ $(function() {
 	$(`#range_max`).on('change', (e) => {
 		let obj = $(e.currentTarget);
 		let target = $(`#range_min`);
-		let newMax = obj.val();
+		let newMax = obj.val() ?? 0;
 
-		newMax = newMax.match(/^-?\d+$/g).length > 0 ? parseInt(newMax) : parseInt(target.attr('max'));
+		newMax = (newMax.match(/^-?\d+$/g)?.length ?? 0) > 0 ? parseInt(newMax) : parseInt(target.attr('max'));
 
 		target.attr('max', newMax);
 	}).trigger('change');
@@ -94,13 +95,20 @@ $(function() {
 
 	// FORM RESETTER
 	$(`#resetForm`).on('click', (e) => {
-		$(`string-card.show`).removeClass(`show`);
-		$(`[name]`).removeClass(`is-valid is-invalid`);
-		$(`form`)[0].reset();
+		$(`#restorePassView`).trigger('click')
+			.remove();
+		let useRange = $(`#useRange`);
 
-		$(`#generated_string`).text("")
-			.closest(`string-card`)
-			.removeClass(`show`);
+		if (useRange.prop(`checked`)) {
+			useRange.prop(`checked`, false).trigger(`change`);
+		}
+
+		$(`[name]`).removeClass(`is-valid is-invalid`)
+			.closest(`form`)
+			.each((k, v) => v.reset());
+
+		let gstr = $(`#generated_string`);
+		animateText(gstr, '');
 
 		SwalFlash.success("Form Reset");
 	});
@@ -140,7 +148,15 @@ $(function() {
 	});
 });
 
-window.validate = function(form) {
+/**
+ * Validates a single form and returns whether they passed the validation or not. Since this
+ * is only used in a very specific instance, the rules and messages are all from a single
+ * source.
+ *
+ * @param {HTMLFormElement|JQuery} form A single form element that will be validated. It could be an `HTMLFormElement` object or a `JQuery` HTML object.
+ * @returns boolean Returns `true` if the form passes validation; `false` otherwise.
+ */
+const validate = function(form) {
 	// Capture all inputs
 	let fields = $(`[name]`).not(`:disabled, [disabled]`);
 	let valids = $(`input:valid`).not(`:disabled, [disabled]`);
@@ -151,22 +167,23 @@ window.validate = function(form) {
 
 	// Update some rule values
 	let variableRule = ['range_min', 'range_max'];
+	let modVRules = JSON.parse(JSON.stringify(validation.rules));
 	for (let r of variableRule) {
-		validation.rules[r].find((v, k) => {
+		modVRules[r].find((v, k) => {
 			if (v.match(/\$\w+/g)) {
 				let targetKey = v.match(/(\$)(\w+)/)[2];
 
 				if (!Object.keys(validation.values).includes(targetKey))
 					return;
 
-				validation.rules[r][k] = v.replace(/(\$)(\w+)/, validation.values[targetKey]);
+				modVRules[r][k] = v.replace(/(\$)(\w+)/, validation.values[targetKey]);
 			}
 		});
 	}
 
 	let validator = new Validator(
 		validation.values,
-		validation.rules,
+		modVRules,
 		validation.message
 	);
 
@@ -230,9 +247,12 @@ window.validate = function(form) {
 
 		regex += typeof values.use_range == 'undefined' ? `{${values.length}}` : `{${values.range_min},${values.range_max}}`;
 
-		$(`#generated_string`)
-			.text(new RandExp(regex).gen())
-			.closest(`.string-card`)
+		let gstr = $(`#generated_string`);
+		let newStr = new RandExp(regex).gen();
+
+		animateText(gstr, newStr);
+
+		gstr.closest(`.string-card`)
 			.addClass(`show`)
 			.find(`.minimize, .maximize`)
 			.addClass(`minimize`).removeClass(`maximize`)
@@ -263,4 +283,78 @@ window.validate = function(form) {
 				);
 		}
 	}
+
+	return validator.fails();
+}
+
+/**
+ * Animates the texts as if they're being cleared and typed in using a keyboard. This animation is
+ * pure JavaScript and thus, really has no support for CSS preferences. However, due to considerations
+ * they are applied and can also be skipped via a click inside the container.
+ *
+ * @param {HTMLElement|JQuery} container An element where the text animation will be applied to.
+ * @param {string} newStr The new string that will be "typed" in place.
+ * @param {number} baseInterval **(Optional)** The approximate interval between changes. It is counted in milliseconds. Default is 25.5 milliseconds.
+ */
+const animateText = (container, newStr, baseInterval = 25.5) => {
+	let isVanilla = typeof container.textContent === "undefined" ? false : true;
+
+	// Use the plain old text replace instead of animating if the users prefers reduced motions.
+	if (window.matchMedia(`(prefers-reduced-motion: reduce)`).matches) {
+		if (isVanilla)	container.textContent = newStr;
+		else			container.text(newStr);
+
+		return;
+	}
+
+	// newStr fallbacks
+	newStr = typeof newStr === 'string' ? newStr : '';
+
+	// Base variables
+	let oldStr = container.textContent ?? container.text();
+	let oldInterval = baseInterval * 10 / oldStr.length;
+	let newInterval = baseInterval * 10 / newStr.length;
+
+	// Control variables
+	let timers = [];
+
+	let skipTextAnimation = () => {
+		let timer;
+		while(timer = timers.pop()) {
+			clearTimeout(timer);
+		}
+
+		if (isVanilla)	container.textContent = newStr;
+		else			container.text(newStr);
+	}
+
+	if (isVanilla)	container.addEventListener(`click`, skipTextAnimation);
+	else			container.on(`click`, skipTextAnimation);
+
+	for (let i = 0; i < oldStr.length; i++) {
+		timers.push(
+			setTimeout(() => {
+				if (isVanilla)	container.textContent = container.textContent.slice(0, -1);
+				else			container.text(container.text().slice(0, -1));
+			}, oldInterval * i)
+		);
+	}
+
+	timers.push(
+		setTimeout(() => {
+			for (let i = 0; i < newStr.length; i++) {
+				timers.push(
+					setTimeout(() => {
+						if (isVanilla)	container.textContent = container.textContent + newStr[i];
+						else			container.text(container.text() + newStr[i]);
+					}, newInterval * i)
+				);
+
+				if (i >= newStr.length - 1) {
+					if (isVanilla)	container.removeEventListener(`click`, skipTextAnimation);
+					else			container.off(`click`, skipTextAnimation);
+				}
+			}
+		}, oldInterval * oldStr.length)
+	);
 }
